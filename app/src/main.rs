6037748621e585b9bed71596e5936877e16468b7
Red Rapious <red.rapious@gmail.com>
2023-08-25 23:17:43 +0200

    Moved code to `lib-ray-tracer`

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,8 @@
-use image::{Rgba, DynamicImage, GenericImage};
-use progress_bar::{init_progress_bar, set_progress_bar_action, Color, Style, inc_progress_bar, finalize_progress_bar};
+use lib_ray_tracer::render_image;
+
 
 fn main() {
-    // Construct a new by repeated calls to the supplied closure.
-    let mut img = DynamicImage::new_rgb8(256, 256);
-        
-    init_progress_bar(256);
-    set_progress_bar_action("Rendering", Color::Blue, Style::Bold);
-    for y in 0..img.height() {
-        inc_progress_bar();
-        for x in 0..img.width() {
-            img.put_pixel(x, y, Rgba([x as u8, y as u8, 0, 1]));
-        }
-    }
-    finalize_progress_bar();
+    let img = render_image();
 
     img.save("generated_images/hello_world.png").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use image::{Rgba, DynamicImage, GenericImage};
+use progress_bar::{init_progress_bar, set_progress_bar_action, Color, Style, inc_progress_bar, finalize_progress_bar};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn render_image() -> DynamicImage {
+    let mut img = DynamicImage::new_rgb8(256, 256);
+        
+    init_progress_bar(256);
+    set_progress_bar_action("Rendering", Color::Blue, Style::Bold);
+    for y in 0..img.height() {
+        inc_progress_bar();
+        for x in 0..img.width() {
+            img.put_pixel(x, y, Rgba([x as u8, y as u8, 0, 1]));
+        }
     }
+    finalize_progress_bar();
+    img
 }
