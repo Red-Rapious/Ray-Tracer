66a9115957b8751e6a92ab800bf370096eaa3fd2
Red Rapious <red.rapious@gmail.com>
2023-08-28 16:22:47 +0200

    Added refraction

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,10 @@
-use lib_ray_tracer::{camera::{Camera, self}, geometry::Sphere, world::World, Renderer, material::Material::{Lambertian, Metal}};
+use lib_ray_tracer::{
+    camera::{self, Camera},
+    geometry::Sphere,
+    material::Material,
+    world::World,
+    Renderer,
+};
 use nalgebra::{Point3, Vector3};
 
 fn main() {
@@ -12,24 +18,30 @@ fn main() {
         Point3::from([0.0, 0.0, 0.0]),
         100,
         10,
-        camera::Gamma::Gamma2
+        camera::Gamma::Gamma2,
     );
 
-    let ground_mat = Lambertian(Vector3::new(0.8, 0.8, 0.0));
-    let center_mat = Lambertian(Vector3::new(0.7, 0.3, 0.3));
-    let left_mat = Metal(Vector3::new(0.8, 0.8, 0.8), 0.3);
-    let right_mat = Metal(Vector3::new(0.8, 0.6, 0.2), 1.0);
+    let ground_mat = Material::Lambertian(Vector3::new(0.8, 0.8, 0.0));
+    //let center_mat = Lambertian(Vector3::new(0.7, 0.3, 0.3));
+    let center_mat = Material::Dielectric(1.5);
+    //let left_mat = Metal(Vector3::new(0.8, 0.8, 0.8), 0.3);
+    let left_mat = Material::Dielectric(1.5);
+    let right_mat = Material::Metal(Vector3::new(0.8, 0.6, 0.2), 1.0);
 
     let mut world = World::empty();
-    world.add(Sphere::new(Point3::new( 0.0, -100.5, -1.0), 100.0, ground_mat));
-    world.add(Sphere::new(Point3::new( 0.0,    0.0, -1.0),   0.5, center_mat));
-    world.add(Sphere::new(Point3::new(-1.0,    0.0, -1.0),   0.5, left_mat));
-    world.add(Sphere::new(Point3::new( 1.0,    0.0, -1.0),   0.5, right_mat));
+    world.add(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        ground_mat,
+    ));
+    world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, center_mat));
+    world.add(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, left_mat));
+    world.add(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, right_mat));
 
     let renderer = Renderer::new(aspect_ratio, image_width, camera);
     //let img = renderer.render_image(&world);
     let img = renderer.render_parallel_image(&world);
 
-    img.save("generated_images/13_fuzzed_metal_large.png")
+    img.save("generated_images/14_pure_glass_refraction.png")
         .unwrap();
 }
--- src/material.rs
+++ src/material.rs
@@ -9,6 +9,7 @@ pub enum Material {
     Lambertian(Vector3<f64>),
     Hemisphere(Vector3<f64>),
     Metal(Vector3<f64>, f64),
+    Dielectric(f64),
 }
 
 impl Material {
@@ -51,6 +52,21 @@ impl Material {
                 // If the scattered ray is below the surface, absorb it (return false)
                 direction.dot(&hit_record.normal) > 0.0
             }
+            Dielectric(index) => {
+                *attenuation = Vector3::new(1.0, 1.0, 1.0);
+                let refraction_ration = match hit_record.front_face {
+                    true => 1.0 / index, // ray goes from air to the dielectric
+                    false => index,      // ray goes from the dielectric to the air
+                };
+
+                let unit_direction = ray_in.direction().normalize();
+                let refracted_direction =
+                    refract(&unit_direction, &hit_record.normal, refraction_ration);
+
+                *scattered_ray = Ray::new(hit_record.hit_point, refracted_direction);
+
+                true
+            }
         }
     }
 }
@@ -91,4 +107,15 @@ fn random_on_hemisphere(normal: &Vector3<f64>, rng: &mut dyn RngCore) -> Vector3
 /// Given a vector and a normal, returns the reflection of the vector on the surface represented by the normal.
 fn reflect(vector: &Vector3<f64>, normal: &Vector3<f64>) -> Vector3<f64> {
     vector - 2.0 * vector.dot(normal) * normal
+}
+
+/// Given a **unit** vector, the normal of a diopter, and the ratio of the indices of both optical mediums,
+/// computes the direction of the refracted ray after crossing the diopter.
+fn refract(unit_vector: &Vector3<f64>, normal: &Vector3<f64>, indices_ratio: f64) -> Vector3<f64> {
+    let cos_theta = -unit_vector.dot(normal);
+
+    let ray_out_perp = indices_ratio * (unit_vector + cos_theta * normal);
+    let ray_out_parallel = -(1.0 - ray_out_perp.norm_squared()).abs().sqrt() * normal;
+
+    ray_out_perp + ray_out_parallel
 }
